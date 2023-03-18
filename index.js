f921ad42d23809ed75164d02135300387189a67a
Saksham511 <sakshamshrestha511@gmail.com>
2023-03-18 17:48:52 +0545

    install express package and use express app to listen at port 4001

---
+++ index.js
@@ -0,0 +1,7 @@
+const express = require('express');
+
+const app = express();
+
+app.listen(4001, console.log("Server is listening at port: 4001"));
+
+console.log ("Hello from backend ecommerce !");
