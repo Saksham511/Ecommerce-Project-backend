caced755919c31037e26246b9116e75941a392eb
Saksham511 <sakshamshrestha511@gmail.com>
2023-03-18 23:28:09 +0545

    json parsor enabled for post request

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ import connectToMongo from "./config/db.js";
 const app = express();
 dotenv.config();
 connectToMongo();
+
+app.use(express.json());
 // app.listen(4001, console.log("Server is listening at port: 4001"));
 app.listen(process.env.PORT, console.log(`Server is listening at port: ${process.env.PORT}`));
 
