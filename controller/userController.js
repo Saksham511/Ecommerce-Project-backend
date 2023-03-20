cb3bac415c2c55d43955d5ef53ee3ede5311ea87
Saksham511 <sakshamshrestha511@gmail.com>
2023-03-20 20:33:56 +0545

    log in success

--- controller/userController.js
+++ controller/userController.js
@@ -21,3 +21,26 @@ export const createUser= async(req,res) => {
         res.status(400).send({message: "Error creating user", error: JSON.stringify(err)});
     }
 };
+
+export const loginAuthentication = async(req,res) => {
+    const {email, password}= req.body;
+    try{
+        const user= await User.findOne({email});
+        if (user){
+            if(password==user.password){
+                res.send({message: "User Authenticated", authenticatedUser: user});
+            }
+            else{
+                res.status(200).send({message: "Wrong Password!"});
+            }
+        }
+        else{
+            console.log("User not found !");
+            res.status(404).send({message: "User not found !"});
+        }
+    }
+    catch(err){
+        console.log(err);
+        res.status(400).send({ message: "Error ! ", error: JSON.stringify(err)});
+    }
+};
