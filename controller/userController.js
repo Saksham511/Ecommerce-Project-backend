d3eaf34b85e921a2f0065d78c414d25fa06edc4a
sandeshstha1222 <sananddesh1029@gmail.com>
2023-03-20 20:44:24 +0545

    farsi

--- controller/userController.js
+++ controller/userController.js
@@ -1,17 +1,16 @@
 import User from "../model/user.js";
 
-export const createUser= async(req,res) => {
-    const {firstName, lastName, email, password, cPassword}= req.body;
-    try{
-        const user = await User.create({
-            firstName,
-            lastName,
-            email,
-            password,
-            cPassword,
-        });
-    }
-    catch(err){
-        console.log(err);
-    }
+export const createUser = async (req, res) => {
+  const { firstName, lastName, email, password, cPassword } = req.body;
+  try {
+    const user = await User.create({
+      firstName,
+      lastName,
+      email,
+      password,
+      cPassword,
+    });
+  } catch (err) {
+    console.log(err);
+  }
 };
