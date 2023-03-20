b4552fdf6f54c5900ce9bbca8c45e01a911c45c6
Saksham511 <sakshamshrestha511@gmail.com>
2023-03-20 22:02:27 +0545

    validaation for email & password during sign up

--- router/userRouter.js
+++ router/userRouter.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import { createUser, loginAuthentication } from "../controller/userController.js";
+import userSchema from "../schemas/user.js";
+import validateData from "../middleware/validation.js";
 
 const userRouter = Router();
 
-userRouter.route("/signup").post(createUser);
+userRouter.route("/signup").post(validateData(userSchema), createUser);
 userRouter.route("/login").get(loginAuthentication);
 
 export default userRouter;
