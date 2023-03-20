8ec7128fcf0e6463c840e1fb7287c4a5342a6fde
Saksham511 <sakshamshrestha511@gmail.com>
2023-03-20 21:02:02 +0545

    route changed for signup and login

--- router/userRouter.js
+++ router/userRouter.js
@@ -3,6 +3,7 @@ import { createUser, loginAuthentication } from "../controller/userController.js
 
 const userRouter = Router();
 
-userRouter.route("/").post(createUser).get(loginAuthentication);
+userRouter.route("/signup").post(createUser);
+userRouter.route("/login").get(loginAuthentication);
 
 export default userRouter;
